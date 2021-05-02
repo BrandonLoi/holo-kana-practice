d8d725f85cb74f6d2063d0b00899733f8c7a6f80
iJustWantFoodPls <>
2021-05-02 17:32:45 -0400

    add pointing Aki images

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -19,9 +19,33 @@
       <br />
       <br />
     </b-container>
-    <b-container class="content" v-else>
-      <QuizArea @returnToOptions="endQuiz" :options="options" />
-    </b-container>
+    <div v-else>
+      <!-- small screen UI -->
+      <b-container class="d-xl-none" fluid>
+        <b-row>
+          <b-col>
+            <b-container class="content">
+              <QuizArea @returnToOptions="endQuiz" :options="options" />
+            </b-container>
+          </b-col>
+        </b-row>
+      </b-container>
+
+      <!-- large screen UI -->
+      <b-container class="d-none d-xl-block" fluid>
+        <b-row>
+          <b-col cols="1" class="d-none d-lg-block" />
+          <b-col cols="6">
+            <b-container class="content">
+              <QuizArea @returnToOptions="endQuiz" :options="options" />
+            </b-container>
+          </b-col>
+          <b-col align-self="center">
+            <b-img fluid :src="require('@/assets/akiPoses/point1.png')" />
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
   </div>
 </template>
 
